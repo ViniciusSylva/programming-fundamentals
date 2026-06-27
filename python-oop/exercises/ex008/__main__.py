43b231125bad170e6c0caaa6bb34a3ab635b5878
Vinicius Sylva <viniciussilva7494@gmail.com>
2026-06-27 16:12:22 -0300

    Trabalhando com Encapsulamento

---
+++ python-oop/exercises/ex008/__main__.py
@@ -0,0 +1,10 @@
+from ex008 import ContaBancaria
+
+def main():
+    c1 = ContaBancaria(111, "Maria", 5000)
+    c1.depositar(500)
+    c1.saldo = 0
+    print(c1)
+
+if __name__ == "__main__":
+    main()
