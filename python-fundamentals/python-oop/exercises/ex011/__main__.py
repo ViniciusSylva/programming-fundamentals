9d6f475c04020d160fa4adab31e29bf4d5cfc25c
Vinicius Sylva <viniciussilva7494@gmail.com>
2026-09-17 20:14:16 -0300

    feat(oop) includes the concept of polymorphism

---
+++ python-fundamentals/python-oop/exercises/ex011/__main__.py
@@ -0,0 +1,9 @@
+print(len("Gustavo Guanabara"))
+print(len(["Curso", "Python"]))
+print(len({"nome": "Maria", "idade": 22}))
+
+
+print(+12)
+print(5.5 + 2.2)
+print("Poli" + "mofirsmo")
+print([1, 2, 3] + [9, 7])
