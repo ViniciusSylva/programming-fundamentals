787a1a865b8aa4bd7eff2fe4ba9f00af3497954c
Vinicius Sylva <viniciussilva7494@gmail.com>
2026-09-20 18:38:47 -0300

    feat(python): implement objects for the challenges

--- python-fundamentals/python-oop/exercises/ex017/__main__.py
+++ python-fundamentals/python-oop/exercises/ex017/__main__.py
@@ -1,7 +1,25 @@
 from classes import *
 
 def main():
-    pass
+
+    a = Numero(200)
+    b = Texto("Vinicius")
+    c = Lista([1, 2, 3])
+    d = Papel()
+    e = Casa()
+
+    tente_dobrar(a)
+    tente_dobrar(b)
+    tente_dobrar(c)
+    tente_dobrar(d)
+    tente_dobrar(e)
+
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    print(e)
+
 
 if __name__ == "__main__":
     main()
