544b07f9b18730b61ca3eb191b42966f20c7022a
Vinicius Sylva <viniciussilva7494@gmail.com>
2026-09-22 19:24:27 -0300

    feat(python): add file for class definitions

---
+++ python-fundamentals/python-oop/challenges/challenge031/__main__.py
@@ -0,0 +1,10 @@
+from classes import *
+
+def main():
+
+    finalizar_compra(Pix(), 1500)
+    finalizar_compra(CartaoCredito(), 9540.84)
+    finalizar_compra(Boleto(), 439.23)
+
+if __name__ == "__main__":
+    main()
---
+++ python-fundamentals/python-oop/challenges/challenge032/__main__.py
@@ -0,0 +1,7 @@
+from classes import *
+
+def main():
+    Erro("Deu ruim...").mostrar()
+
+if __name__ == "__main__":
+    main()
