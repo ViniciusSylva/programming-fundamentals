9f1ea87d2161ea79bb168e9dbb3b6f0ae2033ca1
Vinicius Sylva <viniciussilva7494@gmail.com>
2026-09-23 22:35:54 -0300

    feat(python): add file for class definitions

---
+++ python-fundamentals/python-oop/challenges/challenge033/classes.py
@@ -0,0 +1,37 @@
+class Produto:
+    def __init__(self, nome:str, preco:float=0):
+        self.nome = nome
+        self.preco = preco
+
+    def __str__(self):
+        return f"{self.nome} ({formata_dinheiro(self.preco)})"
+
+
+class Carrinho:
+
+    def __init__(self, produtos:list=None):
+        self.produtos = produtos if produtos else []
+
+    @property
+    def total(self):
+        return sum(p.preco for p in self.produtos)
+
+    def __add__(self, outro):
+        if isinstance(outro, Produto):
+            return Carrinho(self.produtos + [outro])
+        elif isinstance(outro, Carrinho):
+            return Carrinho(self.produtos + outro.produtos)
+        else:
+            raise TypeError("Você tentou adicionar algo inválido ao carrinho")
+
+
+    def __str__(self):
+        linha = "\n" + "-" * 30
+        itens = "\n".join(str(p) for p in self.produtos)
+        return f"{linha}\n{itens}{linha}\nTotal: {formata_dinheiro(self.total)}"
+
+
+def formata_dinheiro(valor:float):
+    import locale
+    locale.setlocale(locale.LC_ALL, "pt_BR.UTF-8")
+    return locale.currency(valor, grouping=True)
