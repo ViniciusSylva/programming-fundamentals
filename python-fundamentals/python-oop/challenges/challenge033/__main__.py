10d595ea44f4688f6d0e24a9f6329dfdf19cd6a1
Vinicius Sylva <viniciussilva7494@gmail.com>
2026-09-23 22:35:45 -0300

    feat(poo): create main to instantiate objects

---
+++ python-fundamentals/python-oop/challenges/challenge033/__main__.py
@@ -0,0 +1,21 @@
+from classes import *
+
+def main():
+    p1 = Produto("Notebook", 8_500)
+    p2 = Produto("Mouse", 250)
+    p3 = Produto("Fone de Ouvido", 450.35)
+
+    c1 = Carrinho()
+    c2 = Carrinho()
+
+    c1 = c1 + p1
+    c1 = c1 + p2
+    c1 = c1 + p3
+
+    c2 = c2 + c1
+
+    print(c1)
+    print(c2)
+
+if __name__ == "__main__":
+    main()
